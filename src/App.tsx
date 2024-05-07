df7e8f1e5608e2a744d82e60b05911f4ddf90dc5
scripto1 <heeyongi@gmail.com>
2024-05-07 16:29:39 +0900

    글로벌스타일,버튼생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from '@/router/routes';
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  :root{
+    --color-yellow: #FCD118;
+    --color-yellow-light: #FEF3C1;
+    --color-green: #93FD98;
+    --color-green-light: #D8FFDA;
+    --color-purple: #9747FF;
+    --color-purple-light: #F0E5FF;
+    --color-gray-dark: #3F3F46;
+    --color-gray-light: #C1C1C1;
+    --color-white: #FFFFFF;
+
+    --font-weight-regular: 400;
+    --font-weight-medium: 500;
+    --font-weight-bold: 700;
+  }
+  body{
+    font-family: "Noto Sans KR", sans-serif;
+  }
+`;
+
 
 const router = createBrowserRouter(routes);
 
@@ -7,6 +32,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <GlobalStyle />
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { PurpleButton, WhiteButton } from './Buttons'
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Heading>Header
+      <PurpleButton>등록</PurpleButton>
+      <WhiteButton>취소</WhiteButton>
+
+    </Heading>
+
+  )
 };
 
 export default Header;
+
+const Heading = styled.div`
+  color: var(--color-purple);
+  font-weight: var(--font-weight-regular);
+`
