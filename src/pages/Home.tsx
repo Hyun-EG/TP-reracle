56a623165523d2d77499255627fd4875c67a96fc
scripto1 <heeyongi@gmail.com>
2024-05-09 16:34:29 +0900

    feat: 헤더네비위치정리v2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import { Layout } from '@/components/layout/Layout';
 import Nav from '@/components/Nav/Nav'
 import Header from '@/components/Header/Header'
-import CategoryHY from '@/components/CategoryHY/CategoryHY'
-// import { Category } from '@/components/Category';
+// import CategoryHY from '@/components/CategoryHY/CategoryHY'
+import { Category } from '@/components/Category';
 
 export const Home = () => {
   return (
     <>  
       <Layout>
         <Header />
-        <CategoryHY />
-        {/* <Category /> */}
+        {/* <CategoryHY /> */}
+        <Category />
         <Nav />             
       </Layout>
     </>
