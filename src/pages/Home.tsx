6f3cb02044c094ca55dd6fb45d3932e08cd9faa7
scripto1 <heeyongi@gmail.com>
2024-05-11 00:24:08 +0900

    feat:subcategory/detailpage완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { Layout } from '@/components/layout/Layout';
 import Nav from '@/components/Nav/Nav'
 import Header from '@/components/Header/Header'
 import CategoryHY from '@/components/CategoryHY/CategoryHY'
+import SubCategoryHY from '@/components/CategoryHY/SubCategoryHY'
+import DetailPageHY from '@/components/CategoryHY/DetailPageHY';
 // import { Category } from '@/components/Category';
 
 export const Home = () => {
@@ -9,7 +11,9 @@ export const Home = () => {
     <>  
       <Layout>
         <Header />
-        <CategoryHY />
+        <DetailPageHY />
+        {/* <SubCategoryHY /> */}
+        {/* <CategoryHY /> */}
         {/* <Category /> */}
         <Nav />             
       </Layout>
