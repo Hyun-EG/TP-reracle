74216dc5d7154371e231e5d86d4493c7110dcb9c
seonghyun <codiee@naver.com>
2024-05-15 13:37:29 +0900

    feature: 로딩 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable arrow-body-style */
+
+import styled from 'styled-components';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from '@/router/routes';
 import GlobalStyles from '@/styles/GlobalStyles';
 import '@shoelace-style/shoelace/dist/themes/light.css';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-import styled from 'styled-components';
+
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/');
 
 const router = createBrowserRouter(routes);
