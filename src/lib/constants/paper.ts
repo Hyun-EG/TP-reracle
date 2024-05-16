afa94c02e2135f3ca92b4c695c49d60b821ec2f0
YuHyeonWook <yho7955@naver.com>
2024-05-16 13:48:49 +0900

    refactor: 불필요한 파일 삭제

--- src/lib/constants/paper.ts
+++
@@ -1,5 +0,0 @@
-// export const paper = [
-//   { id: 'newspaper', img: 'newspaper', name: '신문지' },
-//   { id: 'ricebag', img: 'ricebag', name: '쌀포대' },
-//   { id: 'notebook', img: 'notebook', name: '수첩' },
-// ];
