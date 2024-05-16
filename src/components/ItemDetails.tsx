c0fa324506e1f23de4c37da131ceda645f2033a9
YuHyeonWook <yho7955@naver.com>
2024-05-16 13:47:22 +0900

    feat: ItemDeatils 컴포넌트 추가

--- src/components/ItemDetails.tsx
+++ src/components/ItemDetails.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import { categories } from '@/lib/constants/categories';
+import { useParams } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 
 const ItemDetails = () => {
-  return <Layout>ItemDetails</Layout>;
+  const { categoryId, itemId } = useParams();
+  const category = categories.find((category) => category.id === categoryId);
+  const item = category.items && category.items.find((item) => item.id === itemId);
+
+  return (
+    <Layout>
+      <h2>{item.name}</h2>
+      <img src={`/images/${item.img}.png`} alt={item.name} />
+      <p>{item.disposalMethod}</p>
+    </Layout>
+  );
 };
 
 export default ItemDetails;
