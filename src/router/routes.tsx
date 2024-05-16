f6c7339ef1eef906fbba47c0c6ccb74d61f3a0cc
YuHyeonWook <yho7955@naver.com>
2024-05-16 13:13:39 +0900

    refactor: 불필요한 import문 삭제

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,5 @@
 import { Home, Login, NotFound, SignUp, UserProfile } from '@/pages';
 import { PATH } from '@/lib/constants/path';
-import { Electronics, Clothes, Paper } from '@/components/detailItems';
-import Refrigerator from '@/components/disposalGuide/Refrigerator';
-import Newspaper from '@/components/disposalGuide/Newspaper';
-import RiceBig from '@/components/disposalGuide/RiceBig';
-import { categories } from '@/lib/constants/categories';
-import Cate from '@/components/Cate';
 import CateItems from '@/components/CateItems';
 import ItemDetails from '@/components/ItemDetails';
 
