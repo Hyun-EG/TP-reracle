f36dcd7eb5fac72ba0485f4ab47f9420c35fe052
YuHyeonWook <yho7955@naver.com>
2024-05-17 10:46:13 +0900

    feat: zustand를 활용해서 검색기록 저장소 만듬

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { findMatchingItem } from '@/components/findMatchingItem';
+import { useSearchStore } from '@/lib/store/useSearchStore';
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+  const { addSearchHistory } = useSearchStore();
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+  const handleSearch = (event) => {
+    event.preventDefault();
     const matchedItem = findMatchingItem(searchQuery);
     if (matchedItem) {
       navigate(`/${matchedItem.categoryId}/${matchedItem.id}`);
+      addSearchHistory(searchQuery);
     } else {
       // 검색 결과 없음 처리
+      alert('검색 결과가 없습니다.');
     }
     setSearchQuery('');
   };
