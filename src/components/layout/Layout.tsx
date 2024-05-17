0c9b75f771a8ef12a5b8a50a4c2cdc5787c47d7b
YuHyeonWook <yho7955@naver.com>
2024-05-17 11:01:10 +0900

    refactor: 병합 후 수정안된 부분 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,13 @@
-import { styled } from 'styled-components';
 import Header from '@/components/Header';
 import Nav from '@/components/Nav/Nav';
 import { layoutProps } from '@/lib/types/layoutProps';
 
 export const Layout = ({ children }: layoutProps) => {
   return (
+    <>
       <Header />
       {children}
-<Nav />
+      <Nav />
+    </>
   );
 };
