36b85ae03b3c880f2f911b355d8b5eab1a806ce4
seonghyun <codiee@naver.com>
2024-05-24 02:10:50 +0900

    docs: 카테고리 종량제 부분 추가 + 이미지 추가

--- src/lib/constants/wasteCategoryItemsImages.ts
+++ src/lib/constants/wasteCategoryItemsImages.ts
@@ -18,8 +18,212 @@ import spray from '@/assets/images/detailItemImages/spray.png';
 import beverageCan from '@/assets/images/detailItemImages/beverageCan.png';
 import cannedFood from '@/assets/images/detailItemImages/cannedFood.png';
 import milkpack from '@/assets/images/detailItemImages/milkpack.png';
+import dishcloth from '@/assets/images/detailItemImages/dishcloth.png';
+import eggshell from '@/assets/images/detailItemImages/eggshell.png';
+import golfBall from '@/assets/images/detailItemImages/golfBall.png';
+import rubberGlove from '@/assets/images/detailItemImages/rubberGlove.png';
+import scissors from '@/assets/images/detailItemImages/scissors.png';
+import wig from '@/assets/images/detailItemImages/wig.png';
+import compass from '@/assets/images/detailItemImages/compass.png';
+import woodenLadle from '@/assets/images/detailItemImages/woodenLadle.png';
+import fallenLeaves from '@/assets/images/detailItemImages/fallenLeaves.png';
+import potLid from '@/assets/images/detailItemImages/potLid.png';
+import sickleHandle from '@/assets/images/detailItemImages/sickleHandle.png';
+import treeTrunk from '@/assets/images/detailItemImages/treeTrunk.png';
+import woodenChopsticks from '@/assets/images/detailItemImages/woodenChopsticks.png';
+import woodPiece from '@/assets/images/detailItemImages/woodPiece.png';
+import axHandle from '@/assets/images/detailItemImages/axHandle.png';
+import magnifyingGlasses from '@/assets/images/detailItemImages/magnifyingGlasses.png';
+import woodenCuttingBoard from '@/assets/images/detailItemImages/woodenCuttingBoard.png';
+import lab from '@/assets/images/detailItemImages/lab.png';
+import lighter from '@/assets/images/detailItemImages/lighter.png';
+import racket from '@/assets/images/detailItemImages/racket.png';
+import businessCardWallet from '@/assets/images/detailItemImages/businessCardWallet.png';
+import cottonSwab from '@/assets/images/detailItemImages/cottonSwab.png';
+import disposableRazor from '@/assets/images/detailItemImages/disposableRazor.png';
+import fountainPen from '@/assets/images/detailItemImages/fountainPen.png';
+import hat from '@/assets/images/detailItemImages/hat.png';
+import mask from '@/assets/images/detailItemImages/mask.png';
+import mousePad from '@/assets/images/detailItemImages/mousePad.png';
+import razor from '@/assets/images/detailItemImages/razor.png';
+import strawHat from '@/assets/images/detailItemImages/strawHat.png';
+import wetTissue from '@/assets/images/detailItemImages/wetTissue.png';
+import wood from '@/assets/images/detailItemImages/wood.png';
+import badmintonBall from '@/assets/images/detailItemImages/badmintonBall.png';
+import brush from '@/assets/images/detailItemImages/brush.png';
+import checkerboard from '@/assets/images/detailItemImages/checkerboard.png';
+import comb from '@/assets/images/detailItemImages/comb.png';
+import cushion from '@/assets/images/detailItemImages/cushion.png';
+import inkStone from '@/assets/images/detailItemImages/inkStone.png';
+import pen from '@/assets/images/detailItemImages/pen.png';
+import thermos from '@/assets/images/detailItemImages/thermos.png';
+import vinylCoatedPaper from '@/assets/images/detailItemImages/vinylCoatedPaper.png';
+import awl from '@/assets/images/detailItemImages/awl.png';
+import charcoal from '@/assets/images/detailItemImages/charcoal.png';
+import cotton from '@/assets/images/detailItemImages/cotton.png';
+import fishBone from '@/assets/images/detailItemImages/fishBone.png';
+import markerPen from '@/assets/images/detailItemImages/markerPen.png';
+import vinylFlooring from '@/assets/images/detailItemImages/vinylFlooring.png';
+import matches from '@/assets/images/detailItemImages/matches.png';
+import mechanicalPencil from '@/assets/images/detailItemImages/mechanicalPencil.png';
+import photoPaper from '@/assets/images/detailItemImages/photoPaper.png';
+import picture from '@/assets/images/detailItemImages/picture.png';
+import plant from '@/assets/images/detailItemImages/plant.png';
+import scrubbers from '@/assets/images/detailItemImages/scrubbers.png';
+import shoes from '@/assets/images/detailItemImages/shoes.png';
+import sponge from '@/assets/images/detailItemImages/sponge.png';
+import watch from '@/assets/images/detailItemImages/watch.png';
+import airMat from '@/assets/images/detailItemImages/airMat.png';
+import album from '@/assets/images/detailItemImages/album.png';
+import baseballBall from '@/assets/images/detailItemImages/baseballBall.png';
+import baseballGlove from '@/assets/images/detailItemImages/baseballGlove.png';
+import bedding from '@/assets/images/detailItemImages/bedding.png';
+import candle from '@/assets/images/detailItemImages/candle.png';
+import doll from '@/assets/images/detailItemImages/doll.png';
+import icePack from '@/assets/images/detailItemImages/icePack.png';
+import instrument from '@/assets/images/detailItemImages/instrument.png';
+import pencil from '@/assets/images/detailItemImages/pencil.png';
+import pencilSharpener from '@/assets/images/detailItemImages/pencilSharpener.png';
+import petToiletSeat from '@/assets/images/detailItemImages/petToiletSeat.png';
+import photoFrame from '@/assets/images/detailItemImages/photoFrame.png';
+import umbrella from '@/assets/images/detailItemImages/umbrella.png';
+import woodenBaseballBat from '@/assets/images/detailItemImages/woodenBaseballBat.png';
+import yogaMat from '@/assets/images/detailItemImages/yogaMat.png';
+import carvingKnife from '@/assets/images/detailItemImages/carvingKnife.png';
+import electricalCord from '@/assets/images/detailItemImages/electricalCord.png';
+import electricToothbrush from '@/assets/images/detailItemImages/electricToothbrush.png';
+import electronicPiano from '@/assets/images/detailItemImages/electronicPiano.png';
+import eraser from '@/assets/images/detailItemImages/eraser.png';
+import glue from '@/assets/images/detailItemImages/glue.png';
+import magnet from '@/assets/images/detailItemImages/magnet.png';
+import paperDiapers from '@/assets/images/detailItemImages/paperDiapers.png';
+import tapeMeasure from '@/assets/images/detailItemImages/tapeMeasure.png';
+import toys from '@/assets/images/detailItemImages/toys.png';
+import teaIngredients from '@/assets/images/detailItemImages/teaIngredients.png';
+import thermometer from '@/assets/images/detailItemImages/thermometer.png';
+import weightMeasuringMachine from '@/assets/images/detailItemImages/weightMeasuringMachine.png';
+import carpet from '@/assets/images/detailItemImages/carpet.png';
+import coatedPaper from '@/assets/images/detailItemImages/coatedPaper.png';
+import coffeeBeanIngredients from '@/assets/images/detailItemImages/coffeeBeanIngredients.png';
+import contactLenses from '@/assets/images/detailItemImages/contactLenses.png';
+import corkscrew from '@/assets/images/detailItemImages/corkscrew.png';
+import corkStopper from '@/assets/images/detailItemImages/corkStopper.png';
+import curtain from '@/assets/images/detailItemImages/curtain.png';
+import powerSocket from '@/assets/images/detailItemImages/powerSocket.png';
+import soccer from '@/assets/images/detailItemImages/soccer.png';
+import nipple from '@/assets/images/detailItemImages/nipple.png';
+import crayon from '@/assets/images/detailItemImages/crayon.png';
+import tent from '@/assets/images/detailItemImages/tent.png';
+import denture from '@/assets/images/detailItemImages/denture.png';
+import teaBag from '@/assets/images/detailItemImages/teaBag.png';
+import pineapplePeel from '@/assets/images/detailItemImages/pineapplePeel.png';
+import photoFilm from '@/assets/images/detailItemImages/photoFilm.png';
+import hotPack from '@/assets/images/detailItemImages/hotPack.png';
+import helmet from '@/assets/images/detailItemImages/helmet.png';
 
 export const wasteCategoryItemsImages: { [key: string]: string } = {
+  helmet,
+  hotPack,
+  photoFilm,
+  pineapplePeel,
+  teaBag,
+  denture,
+  tent,
+  crayon,
+  nipple,
+  soccer,
+  cushion,
+  powerSocket,
+  curtain,
+  corkStopper,
+  corkscrew,
+  contactLenses,
+  coffeeBeanIngredients,
+  coatedPaper,
+  carpet,
+  weightMeasuringMachine,
+  thermometer,
+  teaIngredients,
+  bedding,
+  toys,
+  tapeMeasure,
+  paperDiapers,
+  magnet,
+  glue,
+  eraser,
+  electronicPiano,
+  electricToothbrush,
+  electricalCord,
+  carvingKnife,
+  yogaMat,
+  woodenBaseballBat,
+  umbrella,
+  photoFrame,
+  petToiletSeat,
+  pencilSharpener,
+  pencil,
+  instrument,
+  icePack,
+  doll,
+  candle,
+  bedding,
+  baseballGlove,
+  baseballBall,
+  album,
+  airMat,
+  watch,
+  sponge,
+  shoes,
+  scrubbers,
+  plant,
+  picture,
+  photoPaper,
+  mechanicalPencil,
+  matches,
+  markerPen,
+  fishBone,
+  cotton,
+  charcoal,
+  awl,
+  vinylFlooring,
+  vinylCoatedPaper,
+  thermos,
+  pen,
+  inkStone,
+  cushion,
+  comb,
+  checkerboard,
+  brush,
+  badmintonBall,
+  wood,
+  wetTissue,
+  strawHat,
+  razor,
+  mousePad,
+  mask,
+  hat,
+  fountainPen,
+  disposableRazor,
+  cottonSwab,
+  businessCardWallet,
+  racket,
+  lighter,
+  lab,
+  woodenCuttingBoard,
+  magnifyingGlasses,
+  axHandle,
+  woodPiece,
+  woodenChopsticks,
+  treeTrunk,
+  sickleHandle,
+  potLid,
+  fallenLeaves,
+  compass,
+  woodenLadle,
+  wig,
+  scissors,
+  rubberGlove,
+  golfBall,
   refrigerator,
   washingmachine,
   tv,
@@ -40,4 +244,6 @@ export const wasteCategoryItemsImages: { [key: string]: string } = {
   spray,
   cannedFood,
   milkpack,
+  dishcloth,
+  eggshell,
 };
